963f10d847902846910b96c3cf129c2b29838b7f
HamdaouiAbdelhake <hamdaouiabdelhake@gmail.com>
2024-11-17 18:59:19 +0300

    Added init file for services to access service methods

---
+++ backend/app/services/__init__.py
@@ -0,0 +1,22 @@
+# from .author_service import AuthorService
+# from .book_service import BookService
+# from .comment_service import CommentService
+# from .genre_service import GenreService
+# from .list_service import ListService
+from .progress_service import ProgressService
+from .review_service import ReviewService
+# from .user_book_service import UserBookService
+from .user_service import UserService, UserNotFoundError
+
+__all__ = [
+    # "AuthorService",
+    # "BookService",
+    # "CommentService",
+    # "GenreService",
+    # "ListService",
+    "ProgressService",
+    "ReviewService",
+    # "UserBookService",
+    "UserService",
+    "UserNotFoundError",
+]
