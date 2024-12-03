7dde69a9367f4b23c56c86e5c13e8e52b8ab502b
HamdaouiAbdelhake <hamdaouiabdelhake@gmail.com>
2024-12-03 20:15:54 +0300

    minor changes

--- backend/app/services/author_service.py
+++ backend/app/services/author_service.py
@@ -3,12 +3,13 @@ from psycopg2.extras import RealDictCursor
 from app.utils.db import get_db_connection
 from app.models import Author
 
+
 class AuthorService:
-    
+
     @staticmethod
     def get_author(authorid):
         conn = get_db_connection()
-        cursor = conn.cursor(cursor_factory = RealDictCursor)
+        cursor = conn.cursor(cursor_factory=RealDictCursor)
         try:
             cursor.execute(
                 "SELECT * FROM authors WHERE authorid = %s",
@@ -29,7 +30,7 @@ class AuthorService:
     @staticmethod
     def add_author(author: Author):
         conn = get_db_connection()
-        cursor = conn.cursor(cursor_factory=RealDictCursor)   
+        cursor = conn.cursor(cursor_factory=RealDictCursor)
         prompt = """
         INSERT INTO authors (authorid, name, wiki_link, image, bio)
         VALUES (%s,%s,%s,%s,%s);
@@ -38,11 +39,13 @@ class AuthorService:
         try:
             cursor.execute(
                 prompt,
-                author.authorID,
-                author.name,
-                author.wiki_link,
-                author.image,
-                author.bio
+                (
+                    author.authorid,
+                    author.name,
+                    author.wiki_link,
+                    author.image,
+                    author.description,
+                ),
             )
             conn.commit()
         except Exception as e:
@@ -63,12 +66,12 @@ class AuthorService:
             cursor.execute(
                 prompt,
                 (
-                author.name,
-                author.wiki_link,
-                author.image,
-                author.bio,
-                author.authorID
-                )
+                    author.name,
+                    author.wiki_link,
+                    author.image,
+                    author.description,
+                    author.authorid,
+                ),
             )
             conn.commit()
         except Exception as e:
@@ -78,14 +81,12 @@ class AuthorService:
             cursor.close()
             conn.close()
 
-    @staticmethod 
+    @staticmethod
     def delete_author(authorid):
         conn = get_db_connection()
         cursor = conn.cursor()
         try:
-            cursor.execute(
-                "DELETE FROM authors WHERE authorid = %s", (authorid)
-            )
+            cursor.execute("DELETE FROM authors WHERE authorid = %s", (authorid))
         except Exception as e:
             print(f"Error: {e}")
             conn.rollback()
