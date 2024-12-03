5ca36080ffe286dc214655bdadacdb823ce951d9
HamdaouiAbdelhake <hamdaouiabdelhake@gmail.com>
2024-12-04 02:30:26 +0300

    added links to header and top

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,17 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 /* Layout elements */
 import TopContent from './components/Layout/TopContent';
-import Header  from './components/Layout/Header';
+import Header from './components/Layout/Header';
 import Footer from './components/Layout/Footer';
 /* Auth pages */
 import Register from './components/Auth/Register';
 import Login from './components/Auth/Login';
-import Logout from './components/Auth/Logout'
+import Logout from './components/Auth/Logout';
 /* Main pages */
-import HomePage from './pages/HomePage'
-import DiscoverPage from './pages/DiscoverPage'
-import CommunityPage from './pages/CommunityPage'
-import AdvancedSearch from './pages/AdvancedSearch'
+import HomePage from './pages/HomePage';
+import DiscoverPage from './pages/DiscoverPage';
+import CommunityPage from './pages/CommunityPage';
+import AdvancedSearch from './pages/AdvancedSearch';
 /* Specific Pages */
 import BookPage from './pages/BookPage';
 import AuthorPage from './pages/AuthorPage';
@@ -22,9 +22,8 @@ import ProfilePage from './pages/ProfilePage';
 import UserPage from './pages/UserPage';
 import UpdateUser from './pages/UpdateUser';
 import DeleteUser from './pages/DeleteUser';
-
-
-
+/* Not Found page */
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
@@ -34,23 +33,25 @@ function App() {
         <Header />
         <Routes>
           {/* Main pages */}
-          <Route path="/" element={<HomePage/>} />
-          <Route path="/discover/:page" element={<DiscoverPage/>} />
-          <Route path="/community" element={<CommunityPage/>} />
-          <Route path="/advanced-search" element={<AdvancedSearch/>} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/discover/:page" element={<DiscoverPage />} />
+          <Route path="/community" element={<CommunityPage />} />
+          <Route path="/advanced-search" element={<AdvancedSearch />} />
           {/* Specific Pages */}
-          <Route path="/book/:bookid" element={<BookPage/>} />
-          <Route path="/author/author:id" element={<AuthorPage/>} />
-          <Route path="/genre/:genreid" element={<GenrePage/>} />
+          <Route path="/book/:bookid" element={<BookPage />} />
+          <Route path="/author/:authorid" element={<AuthorPage />} />
+          <Route path="/genre/:genreid" element={<GenrePage />} />
           {/* Auth Pages */}
-          <Route path="/register" element={<Register/>} />
-          <Route path="/login" element={<Login/>} />
-          <Route path="/logout" element={<Logout/>} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/logout" element={<Logout />} />
           {/* User Pages */}
-          <Route path="/profile" element={<ProfilePage/>} />
-          <Route path="/:username" element={<UserPage/>} />
-          <Route path="/profile/update" element={<UpdateUser/>} />
-          <Route path="/profile/delete" element={<DeleteUser/>} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/user/:username" element={<UserPage />} />
+          <Route path="/profile/update" element={<UpdateUser />} />
+          <Route path="/profile/delete" element={<DeleteUser />} />
+          {/* Non-existent routes */}
+          <Route path="*" element={<NotFound />} />
         </Routes>
         <Footer />
       </div>
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,8 +1,53 @@
+import {
+    Row,
+    Col,
+    Container,
+  } from "reactstrap";
+
 import Tabs from './../components/Tabs'
 
 function HomePage() {
-    return (<Tabs />)
-
+    return (
+    <>
+        <section id="popular-books" class="bookshelf no-padding-bottom no-margin no-padding">
+		<Container >
+			<Row>
+                <Col md={12}>
+                    <div class="section-header align-center">
+                        <h2 class="section-title">Popular Books</h2>
+                    </div>
+                    <Tabs />
+                </Col>                
+            </Row>
+        </Container>
+        </section>
+        <section id="popular-authors" class="bookshelf no-padding-bottom no-margin no-padding">
+		<Container >
+			<Row>
+                <Col md={12}>
+                    <div class="section-header align-center">
+                        <h2 class="section-title">Popular Authors</h2>
+                    </div>
+                    <Tabs />
+                </Col>                
+            </Row>
+        </Container>
+        </section>
+        <section id="popular-genres" class="bookshelf no-padding-bottom no-margin no-padding">
+		<Container >
+			<Row>
+                <Col md={12}>
+                    <div class="section-header align-center">
+                        <h2 class="section-title">Popular Genres</h2>
+                    </div>
+                    <Tabs />
+                </Col>                
+            </Row>
+        </Container>
+        </section>
+        
+    </>
+    )
 }
     
 export default HomePage;
